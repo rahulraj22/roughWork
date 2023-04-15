16f6f2afba1d412e7fc8402720248b018caa7f98
Rahul Raj <rahulrajsjs22@gmail.com>
2023-04-16 02:06:49 +0530

    reverting to 2f1910dc1e4fca28bb1a0b3d0ac5a17aed2d3b7c

--- hello.py
+++ hello.py
@@ -2,19 +2,3 @@ hello = "Hello World"
 print(hello)
 
 # I using this repo to test it.....
-
-# I am changing this repo locally.
-# I am changing here from remote repo.
-
-
-
-# 2nd update locally....
-
-# 3rd update locally....
-
-# 4th update remotely....
-
-# 5th update remotely....
-
-
-# 6th update remotely....
